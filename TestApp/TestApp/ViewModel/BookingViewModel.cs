813400eb682c863e93680b7141b85a06034742a0
George <George@DESKTOP-E0782RD>
2019-03-07 22:40:22 +0000

    Changed custom control to databindings and a view model to fit MVVM.
    Changed folder name of 'Pages' to 'Views' Implemented refresh functionality.

---
+++ TestApp/TestApp/ViewModel/BookingViewModel.cs
@@ -0,0 +1,67 @@
+﻿using HotelClassLibrary;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    public class BookingViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Booking> Bookings { get; set; }
+
+        //When is refreshing is true the refresh animation continues to trigger
+        //When isRefreshing is false the animation stops
+        private bool _isRefreshing = false;
+        public bool IsRefreshing
+        {
+            get { return _isRefreshing; }
+            set
+            {
+                _isRefreshing = value;
+                OnPropertyChanged(nameof(IsRefreshing));
+            }
+        }
+
+        //Notifies the view when a property is updated
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+  
+        public BookingViewModel()
+        {
+            Bookings = new ObservableCollection<Booking>();
+            getBookings();
+        }
+
+        //Calls the web servive to get a list of all bookings for loggin in customer
+        private async void getBookings()
+        {
+            BookingRestService service = BookingRestService.Instance;
+            Bookings.Clear();
+            var bookings = await service.RefreshDataAsync();
+            foreach (Booking b in bookings)
+                Bookings.Add(b);
+        }
+        //Command to bind refreshing behaviour from view to view model
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    IsRefreshing = true; //Start refreshing animation
+
+                    getBookings(); //Get bookings for user
+
+                    IsRefreshing = false; //Stop refreshing animation
+                });
+            }
+        }
+    }
+}
---
+++ TestApp/TestApp/Views/BookingsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    public partial class BookingsPage : ContentPage
+    {
+        BookingViewModel bm;
+
+        
+        public BookingsPage()
+        {
+            InitializeComponent();
+            bm = new BookingViewModel();
+            BindingContext = bm;
+        }
+
+        private void BookingListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item != null)
+            {
+                DependencyService.Register<IViewBarcodePage>();
+                string bookingjson = JsonConvert.SerializeObject(e.Item);
+                DependencyService.Get<IViewBarcodePage>().StartNativeIntentOrActivity(bookingjson);
+            }
+        }
+
+        
+    }
+}
